56d65cb7ea5a28df767cbc9d9b2931188bcaa01e
lazx450 <amidbagus079@gmail.com>
2026-08-02 18:39:27 +0700

    Add namespace detection in build.gradle.kts
    Automatically set the Android namespace from the manifest if not defined.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,24 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        val androidExtension = project.extensions.findByName("android")
+        if (androidExtension is com.android.build.gradle.BaseExtension) {
+            if (androidExtension.namespace == null) {
+                val manifestFile = file("src/main/AndroidManifest.xml")
+                if (manifestFile.exists()) {
+                    val manifestText = manifestFile.readText()
+                    val match = Regex("package=\"([^\"]+)\"").find(manifestText)
+                    if (match != null) {
+                        androidExtension.namespace = match.groupValues[1]
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
